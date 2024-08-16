45d6ee656ff618887e8bb7f99dec7da45d7327f9
Mariam_Tarek <mariamelsarag44@gmail.com>
2024-08-16 14:54:55 +0300

    create catchAsync function to cate errors

--- Backend/Controller/error-controller.js
+++ Backend/Controller/error-controller.js
@@ -1,3 +1,4 @@
+const httpStatusText = require("../utils/httpStatusText");
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || httpStatusText.FAIL;
