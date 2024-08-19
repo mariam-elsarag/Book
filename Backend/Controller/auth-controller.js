8a9613ea3a0e7713efd77664c8a4b406da672d13
Mariam_Tarek <mariamelsarag44@gmail.com>
2024-08-19 20:49:09 +0300

    isAdmin middle ware and make only admin can add new admin

--- Backend/Controller/auth-controller.js
+++ Backend/Controller/auth-controller.js
@@ -8,86 +8,135 @@ const AppError = require("../utils/appError");
 const httpStatusText = require("../utils/httpStatusText");
 
 const generateToken = (user) => {
-  const token = jwt.sign(
-    { id: user._id, role: user.role },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_EXPIRE_IN,
-    }
-  );
-  return token;
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE_IN,
+  });
 };
-exports.register = CatchAsync(async (req, res, next) => {
-  const { first_name, last_name, email, password } = req.body;
-  let error = [];
-  if (!first_name || !last_name || !email || !password) {
-    if (!first_name) {
-      error.push({ first_name: "First name is required" });
-    }
-    if (!last_name) {
-      error.push({ last_name: "Last name is required" });
-    }
-    if (!email) {
-      error.push({ email: "Email is required" });
-    }
-    if (!password) {
-      error.push({ password: "Password is required" });
-    }
-    return next(new AppError(error, 400));
-  } else {
-    const user = await User.create({ first_name, last_name, email, password });
-    res.status(201).json({ user });
-  }
-});
-exports.login = CatchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
-  let error = [];
-  if (!email || !password) {
-    if (!email) {
-      error.push({ email: "Please enter an email" });
-    }
-    if (!password) {
-      error.push({ password: "Please enter a password" });
-    }
-    return next(new AppError(error, 400));
-  }
-  const user = await User.findOne({ email }).select("+password -__v");
 
-  if (!user || !(await user.checkPassword(password, user.password))) {
-    return next(new AppError("email or password are wrong", 401));
-  }
-
-  const token = generateToken(user);
-
-  res
-    .status(200)
-    .json({ status: httpStatusText.SUCCESS, token, user: user.toJson() });
-});
-exports.protect = CatchAsync(async (req, res, next) => {
-  let token;
-
+const extractToken = (req) => {
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    token = req.headers.authorization.split(" ")[1];
+    return req.headers.authorization.split(" ")[1];
   }
-  if (!token) {
+  return null;
+};
+
+const verifyToken = async (token) => {
+  return await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+};
+
+exports.register = CatchAsync(async (req, res, next) => {
+  const { first_name, last_name, email, password, role } = req.body;
+  const errors = [];
+
+  if (!first_name) errors.push({ first_name: "First name is required" });
+  if (!last_name) errors.push({ last_name: "Last name is required" });
+  if (!email) errors.push({ email: "Email is required" });
+  if (!password) errors.push({ password: "Password is required" });
+
+  if (errors.length > 0) {
+    return next(new AppError(errors, 400));
+  }
+
+  if (role === "admin") {
+    const token = extractToken(req);
+    if (!token)
+      return next(new AppError("Unauthorized: Access is denied", 401));
+
+    const decoded = await verifyToken(token);
+    if (!decoded)
+      return next(new AppError("Unauthorized: Access is denied", 401));
+
+    const user = await User.findById(decoded.id);
+    if (!user) return next(new AppError("User no longer exists", 404));
+    if (user.checkChangePasswordAfterJWT(decoded.iat)) {
+      return next(new AppError("User recently changed password", 404));
+    }
+
+    if (decoded.role !== "admin") {
+      return next(
+        new AppError(
+          "Access denied: You do not have permission to perform this action",
+          403
+        )
+      );
+    }
+  }
+
+  const user = await User.create({
+    first_name,
+    last_name,
+    email,
+    password,
+    role,
+  });
+  res.status(201).json({ status: httpStatusText.SUCCESS, user });
+});
+
+exports.login = CatchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  const errors = [];
+
+  if (!email) errors.push({ email: "Please enter an email" });
+  if (!password) errors.push({ password: "Please enter a password" });
+
+  if (errors.length > 0) {
+    return next(new AppError(errors, 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password -__v");
+  if (!user || !(await user.checkPassword(password, user.password))) {
+    return next(new AppError("Email or password is incorrect", 401));
+  }
+
+  const token = generateToken(user);
+  res
+    .status(200)
+    .json({ status: httpStatusText.SUCCESS, token, user: user.toJson() });
+});
+
+exports.protect = CatchAsync(async (req, res, next) => {
+  const token = extractToken(req);
+  if (!token) return next(new AppError("Unauthorized: Access is denied", 401));
+
+  const decoded = await verifyToken(token);
+  if (!decoded)
     return next(new AppError("Unauthorized: Access is denied", 401));
+
+  const user = await User.findById(decoded.id);
+  if (!user) return next(new AppError("User no longer exists", 404));
+  if (user.checkChangePasswordAfterJWT(decoded.iat)) {
+    return next(new AppError("User recently changed password", 404));
   }
-  // check token
-  const decode = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  if (!decode) {
-    return next(new AppError("Unauthorized: Access is denied", 401));
-  }
-  // check user exist
-  const user = await User.findById(decode.id);
-  if (!user) {
-    return next(new AppError("User no longer exist", 404));
-  }
-  // check if password change after jwt issue
-  if (user.checkChangePasswordAfterJWT(decode.iat)) {
-    return next(new AppError("User recently change password", 404));
-  }
+
+  req.user = user;
   next();
+});
+
+exports.isAdmin = CatchAsync(async (req, res, next) => {
+  const token = extractToken(req);
+  if (!token) return next(new AppError("Unauthorized: Access is denied", 401));
+
+  const decoded = await verifyToken(token);
+  if (!decoded)
+    return next(new AppError("Unauthorized: Access is denied", 401));
+
+  const user = await User.findById(decoded.id);
+  if (!user) return next(new AppError("User no longer exists", 404));
+  if (user.checkChangePasswordAfterJWT(decoded.iat)) {
+    return next(new AppError("User recently changed password", 404));
+  }
+
+  if (decoded.role === "admin") {
+    return next();
+  } else {
+    return next(
+      new AppError(
+        "Access denied: You do not have permission to perform this action",
+        403
+      )
+    );
+  }
 });
--- Backend/Routes/auth-route.js
+++ Backend/Routes/auth-route.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const multer = require("multer");
-
-const router = express.Router();
-
-// controller
 const authController = require("../Controller/auth-controller");
 
-// multer
+const router = express.Router();
 const upload = multer();
 
+// Routes
 router.route("/register").post(upload.none(), authController.register);
 
 router.route("/login").post(upload.none(), authController.login);
