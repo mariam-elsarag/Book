5b284672e8adc829ff94e9d9b415f553bfa9b0a8
Mariam_Tarek <mariamelsarag44@gmail.com>
2024-09-27 16:31:05 +0300

    order model

--- Backend/Models/Order-model.js
+++ Backend/Models/Order-model.js
@@ -1,5 +1,32 @@
 const mongoose = require("mongoose");
-const orderSchema = new mongoose.Schema({});
-
+const orderSchema = new mongoose.Schema({
+  book: {
+    type: mongoose.Schema.ObjectId,
+    ref: "book",
+    require: [true, "Order must have a book"],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "user",
+    require: [true, "Order must belong to a user"],
+  },
+  price: {
+    type: Number,
+    require: [true, "Order must have a price"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+orderSchema.pre(/^find/, function (next) {
+  this.populate({ path: "user", select: "profile_img first_name last_name" });
+  this.populate({ path: "book", select: "title thumbnail quantity price" });
+  next();
+});
 const Order = mongoose.model("Order", orderSchema, "Order");
 module.exports = Order;
